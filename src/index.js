95371acc95e70e21c8ae6a1ca4f44b13aee8d006
Will Bickford <wbic16@gmail.com>
2024-08-27 19:40:35 -0500

    Added constants

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
+// -----
+// Phext
+// -----
+// (c) 2024 Phext, Inc.
+// Licensed under the MIT License
+//
+// This project was ported from https://github.com/wbic16/libphext-rs/blob/master/src/phext.rs.
+// Web Site: https://phext.io/
+// -----------------------------------------------------------------------------------------------------------
 export class Phext {
 	constructor(subspace) {
 		this.subspace = subspace;
 		this.state = 'unparsed';
+		this.COORDINATE_MINIMUM  = 1;
+    	this.COORDINATE_MAXIMUM  = 100;
+        this.LIBRARY_BREAK       = '\x01'; // 11D Break - replaces start of header
+        this.MORE_COWBELL        = '\x07'; // i've got a fever, and the only prescription...is more cowbell!
+        this.LINE_BREAK          = '\x0A'; // same as plain text \o/
+        this.SCROLL_BREAK        = '\x17'; // 3D Break - replaces End Transmission Block
+        this.SECTION_BREAK       = '\x18'; // 4D Break - replaces Cancel Block
+        this.CHAPTER_BREAK       = '\x19'; // 5D Break - replaces End of Tape
+        this.BOOK_BREAK          = '\x1A'; // 6D Break - replaces Substitute
+        this.VOLUME_BREAK        = '\x1C'; // 7D Break - replaces file separator
+        this.COLLECTION_BREAK    = '\x1D'; // 8D Break - replaces group separator
+        this.SERIES_BREAK        = '\x1E'; // 9D Break - replaces record separator
+        this.SHELF_BREAK         = '\x1F'; // 10D Break - replaces unit separator
+        this.ADDRESS_MICRO_BREAK = '.'; // delimiter for micro-coordinates
+        this.ADDRESS_MACRO_BREAK = '/'; // delimiter for macro-coordinates
+        this.ADDRESS_MACRO_ALT   = ';';   // also allow ';' for url encoding
 	}
 	parse = () => {
 		console.log("Parsing phext!");
--- test-app/index.js
+++ test-app/index.js
@@ -2,3 +2,17 @@ const { Phext } = require("@libphext/core-js");
 const phext = new Phext("hello world");
 phext.parse();
 phext.status();
+function verify(test_name, constant, value) {
+    console.log(`${test_name}: ${constant == value ? 'OK' : 'Failed'}`);
+}
+verify('LB', phext.LIBRARY_BREAK, '\x01');
+verify('MC', phext.MORE_COWBELL, '\x07');
+verify('LF', phext.LINE_BREAK, '\n');
+verify('SB', phext.SCROLL_BREAK, '\x17');
+verify('SN', phext.SECTION_BREAK, '\x18');
+verify('CH', phext.CHAPTER_BREAK, '\x19');
+verify('BK', phext.BOOK_BREAK, '\x1A');
+verify('VM', phext.VOLUME_BREAK, '\x1C');
+verify('CN', phext.COLLECTION_BREAK, '\x1D');
+verify('SR', phext.SERIES_BREAK, '\x1E');
+verify('SF', phext.SHELF_BREAK, '\x1F');
